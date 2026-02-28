78b955fd57b244935a3c49371a42ef02f9c1c18a
Mykola Prybysh <m.prybysh@ukma.edu.ua>
2026-02-28 18:54:48 +0200

    Add HotspotIntegerKeyGenerator

---
+++ src/main/java/ua/edu/ukma/generators/HotspotIntegerKeyGenerator.java
@@ -0,0 +1,25 @@
+package ua.edu.ukma.generators;
+
+import java.util.Random;
+
+public class HotspotIntegerKeyGenerator extends AbstractPrecomputedKeyGenerator<Integer> {
+
+    public HotspotIntegerKeyGenerator(int numberOfKeys, int range, double hotSpotProbability, long seed) {
+        super(computeKeys(numberOfKeys, range, hotSpotProbability, seed));
+    }
+
+    private static Integer[] computeKeys(int numberOfKeys, int range, double hotSpotProbability, long seed) {
+        Random random = new Random(seed);
+        Integer[] keys = new Integer[numberOfKeys];
+        int hotSpotRange = Math.max(range / 100, 1);
+        range = Math.max(range, hotSpotRange + 1);
+        for (int i = 0; i < numberOfKeys; i++) {
+            if (random.nextDouble() < hotSpotProbability) {
+                keys[i] = random.nextInt(hotSpotRange);
+            } else {
+                keys[i] = random.nextInt(hotSpotRange, range);
+            }
+        }
+        return keys;
+    }
+}
